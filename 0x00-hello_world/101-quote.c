879b6692522558e085d992a19f6b5a2b7f7d9e05
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-02-19 18:20:14 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints a line to thae standard error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Kor
+			par, 2015-10-19\r", stdout);
+	return (1);
+}
