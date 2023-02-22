819d791818d3621318bc0f8a7b23aca117e44776
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-02-22 12:55:45 +0100

    10-add.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,6 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-	
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -21,7 +20,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');			
+				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
 			{
